f95e5aeb3940b25994333e382c34e97a9a269f44
QingpingMeng <missing1989@gmail.com>
2018-08-07 22:29:43 -0700

    setting up apollo and other configs

--- next.config.js
+++ next.config.js
@@ -1,3 +1,47 @@
-// next.config.js
 const withTypescript = require('@zeit/next-typescript');
-module.exports = withTypescript();
+const withSass = require('@zeit/next-sass');
+const withImages = require('next-images');
+const path = require('path');
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const port = parseInt(process.env.PORT, 10) || 3000;
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+
+const alias = {
+  'pages': path.resolve(__dirname, './pages'),
+  'libs': path.resolve(__dirname, './libs'),
+  'types': path.resolve(__dirname, './types'),
+  'static': path.resolve(__dirname, './static'),
+  'style': path.resolve(__dirname, './style'),
+  'store': path.resolve(__dirname, './store'),
+  'components': path.resolve(__dirname, './components')
+};
+
+module.exports = withTypescript(withSass(withImages({
+  inlineImageLimit: 10240,
+  webpack: (config, {dev, isServer}) => {
+    for (let p in alias) {
+      config.resolve.alias[p] = alias[p];
+    }
+    if (!dev) {
+      config.devtool = 'source-map';
+    }
+    if (dev && !isServer) {
+      config.plugins.push(new OpenBrowserPlugin({url: `http://localhost:${port}`}));
+    }
+    if (!dev) {
+      config.plugins.push(
+        new SWPrecacheWebpackPlugin({
+          verbose: true,
+          staticFileGlobsIgnorePatterns: [/\.next\//],
+          runtimeCaching: [
+            {
+              handler: 'networkFirst',
+              urlPattern: /^https?.*/
+            }
+          ]
+        })
+      );
+    }
+    return config;
+  }
+})));
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,23 @@
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider, Query } from 'react-apollo';
+import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
-import fetch from 'isomorphic-fetch';
-
-const client = new ApolloClient({
-    uri: 'http://localhost:3000/graphql',
-    fetch
-});
+import withApolloClient from '../lib/with-apollo-client';
 
 const PROFILE_QUERY = gql`
     query RootQuery {
-        queryArticles{
+        queryArticles {
             slug
         }
     }
 `;
 
 const App = () => (
-    <ApolloProvider
-        client={client}
-        children={
-            <Query
-                query={PROFILE_QUERY}
-                fetchPolicy={'cache-and-network'}
-                children={({ loading, error, data: { currentUser } }) => {
-                    if (loading) return <span>loading....</span>;
-                    if (error) return <span>error...</span>;
-                    return <h1>Welcome back {currentUser.firstName}</h1>;
-                }}
-            />
-        }
-    />
+    <Query query={PROFILE_QUERY} fetchPolicy={'cache-and-network'}>
+        {({ loading, error }) => {
+            if (loading) return <span>loading....</span>;
+            if (error) return <span>error...</span>;
+            return <h1>Welcome back</h1>;
+        }}
+    </Query>
 );
 
-export default () => <App />;
+export default withApolloClient(App);
