7e08014ec17216247528fca807098283931c2281
QingpingMeng <missing1989@gmail.com>
2018-09-20 22:55:12 -0700

    hard code blog server endpoint in CI

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -1,6 +1,9 @@
-export const graphQLEndpoint =
-    process.env.graphQLEndpoint || 'http://localhost:3000/graphql';
-export const authSignInEdnpoint =
-    process.env.authSignInEndpoint || 'http://localhost:3000/signin';
+export const serverEndpoint =
+    process.env.CI ? 'http://blog-server/' : 'http://localhost:4000';
+
+export const graphQLEndpoint = `${serverEndpoint}/graphql`;
+
+export const authSignInEdnpoint = `${serverEndpoint}/signin;`;
+
 export const githubClientId =
     process.env.githubClientId || '5a23c310903a1d2675cc';
