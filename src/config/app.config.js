1c7de28b19d9f4428f949fbf84c401aafa592ea0
QingpingMeng <missing1989@gmail.com>
2018-09-20 23:53:57 -0700

    update env vars

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -1,5 +1,5 @@
 export const serverEndpoint =
-    process.env.CI ? 'http://blog-server/' : 'http://localhost:4000';
+    process.env.NODE_ENV === 'production' ? 'http://blog-server/' : 'http://localhost:4000';
 
 export const graphQLEndpoint = `${serverEndpoint}/graphql`;
 
