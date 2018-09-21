e1cee6baa2a6b0b03d65f3f3c08aa9ec8823e2a4
QingpingMeng <missing1989@gmail.com>
2018-09-21 00:01:41 -0700

    small fix

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -1,5 +1,5 @@
 export const serverEndpoint =
-    process.env.NODE_ENV === 'production' ? 'http://blog-server/' : 'http://localhost:4000';
+    process.env.NODE_ENV === 'production' ? 'http://blog-server' : 'http://localhost:4000';
 
 export const graphQLEndpoint = `${serverEndpoint}/graphql`;
 
